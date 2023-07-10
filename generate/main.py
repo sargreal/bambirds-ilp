a0f64280309610b8bde1d9c1e1e009cf91532c7f
Felix Haase <felix.haase@stud.uni-bamberg.de>
2023-07-10 11:17:48 +0000

    update data generation

--- generate/main.py
+++ generate/main.py
@@ -1,24 +1,49 @@
 from testbed import TestBed
+from tqdm import trange, tqdm
 import math
 import argparse
 import random
 import time
+import os
+import shutil
+import pymunk
 
 
 def enclose(preds, pred):
     return [f'{pred}({s}).\n' for s in preds]
 
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+root_directory = os.path.abspath(os.path.join(current_directory, '..'))
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser('generate')
 
-    parser.add_argument('out')
-    parser.add_argument('exs')
+    parser.add_argument('folder', choices=[
+                        'train', 'test'], help='The folder to save the generated files to')
+    parser.add_argument('--suffix', required=True)
     parser.add_argument('--situations', default=1, type=int)
-    parser.add_argument('--head', default = "supports", choices=["supports", "stable"])
-    parser.add_argument('--seed', default = None)
+    parser.add_argument('--head', default="supports",
+                        choices=["supports", "stable"])
+    parser.add_argument('--seed', default=None)
+    parser.add_argument('--world', default="science_birds",
+                        choices=["science_birds", "random", 'all'])
+    parser.add_argument('--filter-supports', default=False, action='store_true', dest='filter_supports',
+                        help='Whether to filter supports for if the supported object would move if the supporter was removed')
 
     args = parser.parse_args()
+    suffix = f'_{args.suffix}' if args.suffix else ''
+    background_file = os.path.join(
+        root_directory, 'data', args.folder, f'bg_{args.head}{suffix}.pl')
+    examples_file = os.path.join(
+        root_directory, 'data', args.folder, f'exs_{args.head}{suffix}.pl')
+
+    img_folder = os.path.join(root_directory, 'data',
+                              args.folder, 'img', f"{args.head}{suffix}")
+    if os.path.exists(img_folder):
+        shutil.rmtree(img_folder)
+    os.makedirs(img_folder)
 
     random.seed(args.seed)
     in_situations = []
@@ -29,10 +54,8 @@ if __name__ == "__main__":
     stable_pos = []
     stable_neg = []
     situations = []
-    materials.append(f'hasMaterial(ground,hill,-100,-1,200,1).\n')
-    shapes.append(f'shape(ground,rect,0,-0.5,0,[1,200,0]).\n')
 
-    for situation in range(args.situations):
+    for situation in trange(args.situations):
 
         current_situation = f'bg_{situation}'
         f'situation({current_situation}).\n'
@@ -43,41 +66,60 @@ if __name__ == "__main__":
             random_remove = True if random.random() > 0.3 else False
             min_percent = random.uniform(0.5, 0.95)
 
-        tb = TestBed(100,100,50,random_remove, min_percent=min_percent, seed=random.random())
-        tb.draw(f'before_{situation}.png')
+        tb = TestBed(100, 100, 50, random_remove,
+                     min_percent=min_percent, seed=random.random())
+        if args.world == "science_birds":
+            tb.create_sb_world()
+        elif args.world == "random":
+            tb.create_random_world()
+        elif args.world == "all":
+            random.choice([tb.create_sb_world, tb.create_random_world])()
+        else:
+            raise Exception("Invalid world type")
+        tb.draw(os.path.join(img_folder, f'sit_{situation}_before.png'))
         supports = tb.get_supports()
+        if args.filter_supports:
+            supports = tb.filter_supports(supports)
+
         neg_supports = []
         for x in range(len(supports)):
-            a = random.randint(0,len(tb.shapes) - 1) if random.random() > 0.2 else 'ground'
-            b = random.randint(0,len(tb.shapes) - 1)
-            while a == b or (a,b) in supports or (a,b) in neg_supports:
-                a = random.randint(0,len(tb.shapes) - 1)
-                b = random.randint(0,len(tb.shapes) - 1)
-            neg_supports.append((a,b))
+            a = random.randint(0, len(tb.shapes) -
+                               1) if random.random() > 0.2 else 'ground'
+            b = random.randint(0, len(tb.shapes) - 1)
+            while a == b or (a, b) in supports or (a, b) in neg_supports:
+                a = random.randint(0, len(tb.shapes) - 1)
+                b = random.randint(0, len(tb.shapes) - 1)
+            neg_supports.append((a, b))
 
         situations = [f'situation({current_situation}).\n']
-        in_situations.append(f'inSituation(ground,{current_situation}).\n')
 
         for i, shape in enumerate(tb.shapes):
             bg_object = f'bg_{situation}_{i}'
-            in_situations.append(f'inSituation({bg_object},{current_situation}).\n')
-            materials.append(f'hasMaterial({bg_object},wood,{round(shape.bb.left)},{round(shape.bb.bottom)},{round(shape.bb.right-shape.bb.left)},{round(shape.bb.top-shape.bb.bottom)}).\n')
+            in_situations.append(
+                f'inSituation({bg_object},{current_situation}).\n')
+            material = "wood" if shape.body.body_type == pymunk.Body.DYNAMIC else "hill"
+            materials.append(
+                f'hasMaterial({bg_object},{material},{round(shape.bb.left)},{round(shape.bb.bottom)},{round(shape.bb.right-shape.bb.left)},{round(shape.bb.top-shape.bb.bottom)}).\n')
 
-            vertices = [f"[{shape.body.position.x + v.x},{shape.body.position.y + v.y}]" for v in shape.get_vertices()]
-            shapes.append(f'shape({bg_object},poly,{shape.body.position.x},{shape.body.position.y},{shape.area},[{len(vertices)},{",".join(vertices)}]).\n')
+            vertices = [
+                f"[{shape.body.position.x + v.x},{shape.body.position.y + v.y}]" for v in shape.get_vertices()]
+            shapes.append(
+                f'shape({bg_object},poly,{shape.body.position.x},{shape.body.position.y},{shape.area},[{len(vertices)},{",".join(vertices)}]).\n')
 
-        for a,b in supports:
+        for a, b in supports:
             if a == 'ground':
                 supports_pos.append(f'supports(ground,bg_{situation}_{b})')
             else:
-                supports_pos.append(f'supports(bg_{situation}_{a},bg_{situation}_{b})')
+                supports_pos.append(
+                    f'supports(bg_{situation}_{a},bg_{situation}_{b})')
 
-        for a,b in neg_supports:
+        for a, b in neg_supports:
             if a == 'ground':
                 supports_neg.append(f'supports(ground,bg_{situation}_{b})')
             else:
-                supports_neg.append(f'supports(bg_{situation}_{a},bg_{situation}_{b})')
-        
+                supports_neg.append(
+                    f'supports(bg_{situation}_{a},bg_{situation}_{b})')
+
         if args.head == 'supports':
             continue
 
@@ -88,16 +130,15 @@ if __name__ == "__main__":
                 stable_pos.append(f'stable(bg_{situation}_{i})')
             elif compares == 'moved':
                 stable_neg.append(f'stable(bg_{situation}_{i})')
-        
-        tb.draw(f'after_{situation}.png')
+
+        tb.draw(os.path.join(img_folder, f'sit_{situation}_after.png'))
 
     supports_pos.sort()
     supports_neg.sort()
     stable_pos.sort()
     stable_neg.sort()
 
-
-    with open(args.out, 'w') as outfile:
+    with open(background_file, 'w') as outfile:
         outfile.writelines(situations)
         outfile.writelines(in_situations)
         outfile.writelines(materials)
@@ -105,7 +146,7 @@ if __name__ == "__main__":
         if args.head == 'stable':
             outfile.writelines([s + '.\n' for s in supports_pos])
 
-    with open(args.exs, 'w') as outfile:
+    with open(examples_file, 'w') as outfile:
         if args.head == 'supports':
             outfile.writelines(enclose(supports_pos, 'pos'))
             outfile.writelines(enclose(supports_neg, 'neg'))
@@ -113,9 +154,17 @@ if __name__ == "__main__":
             outfile.writelines(enclose(stable_pos, 'pos'))
             outfile.writelines(enclose(stable_neg, 'neg'))
 
-
-
-# python3 generate/main.py --situations 4 data/testing/bg_supports.pl data/testing/exs_supports.pl
-# python3 generate/main.py --situations 10 data/learning/bg_supports.pl data/learning/exs_supports.pl
-# python3 generate/main.py --situations 10 --head stable data/learning/bg_stable.pl data/learning/exs_stable.pl
-# python3 generate/main.py --situations 4 --head stable data/testing/bg_stable.pl data/testing/exs_stable.pl
+"""
+python3 generate/main.py --situations 4   --suffix sb     test
+python3 generate/main.py --situations 10  --suffix sb     train
+python3 generate/main.py --situations 4   --world random  --suffix random  test
+python3 generate/main.py --situations 10  --world random  --suffix random  train
+python3 generate/main.py --situations 10  --world all  --suffix all  test
+python3 generate/main.py --situations 10  --world all  --suffix all  train
+python3 generate/main.py --situations 10  --head stable --suffix sb  train
+python3 generate/main.py --situations 4   --head stable --suffix sb  test
+python3 generate/main.py --situations 20  --head stable --world random  --suffix random   train
+python3 generate/main.py --situations 10   --head stable --world random  --suffix random   test
+python3 generate/main.py --situations 20  --head stable --world all  --suffix all   train
+python3 generate/main.py --situations 10   --head stable --world all  --suffix all   test
+"""
--- generate/testbed.py
+++ generate/testbed.py
@@ -7,18 +7,21 @@ from ItarusAves import make_structure, blocks
 import math
 import copy
 
-def distance(coordA,coordB):
+
+def distance(coordA, coordB):
     return math.sqrt((coordA[0]-coordB[0])**2 + (coordA[1]-coordB[1])**2)
 
+
 def coords(object):
-    return (object[1],object[2])
+    return (object[1], object[2])
+
 
 def remove_duplicates(objects):
     result = []
     for i in objects:
         exists = False
         for j in result:
-            dist = distance(coords(i),coords(j))
+            dist = distance(coords(i), coords(j))
             if dist < 0.01:
                 exists = True
                 break
@@ -26,20 +29,72 @@ def remove_duplicates(objects):
             result.append(i)
     return result
 
-def add_itarus_objects_to_space(objects: list,space:pymunk.Space):
+
+def get_itarus_dimensions(object: list[int]) -> tuple[int, int]:
+    if object[0] == 0:
+        return (object[3], object[4])
+    else:
+        return tuple(blocks[str(object[0])])
+
+
+def get_itarus_rotation(object: list[int]) -> float:
+    if object[0] == 0:
+        return object[5]
+    else:
+        return 0.0
+
+
+def add_itarus_objects_to_space(objects: list, space: pymunk.Space):
     shapes = []
     for object in objects:
-        mass = 10.0
-        dimensions = blocks[str(object[0])]
-        moment = pymunk.moment_for_box(mass, dimensions)
-        body = pymunk.Body(mass, moment)
+        dimensions = get_itarus_dimensions(object)
+        if object[0] == 0 and object[6] == pymunk.Body.STATIC:
+            body = pymunk.Body(body_type=pymunk.Body.STATIC)
+            color = (0, 0, 0, 255)
+            friction = 1.0
+        else:
+            mass = 10.0
+            moment = pymunk.moment_for_box(mass, dimensions)
+            body = pymunk.Body(mass, moment)
+            color = (180, 180, 0, 255)
+            friction = 0.3
+        rotation = get_itarus_rotation(object)
+        body.angle = rotation
         body.position = Vec2d(object[1], object[2])
         shape = pymunk.Poly.create_box(body, dimensions)
-        shape.friction = 0.3
-        shape.color = (180, 180, 0, 255)
+        shape.friction = friction
+        shape.color = color
         space.add(body, shape)
         shapes.append(shape)
     return shapes
+
+
+def bounding_box_to_dimensions(bb: pymunk.BB):
+    return (bb.right - bb.left, bb.top - bb.bottom)
+
+
+def extract_itarus_objects_from_space(space: pymunk.Space, objects: list):
+    new_objects = []
+    for shape, object in zip(space.shapes, objects):
+        body = shape.body
+        x = body.position.x
+        y = body.position.y
+        rotation = body.angle
+        width, height = get_itarus_dimensions(object)
+        new_objects.append([0, x, y, width, height, rotation, body.body_type])
+    return new_objects
+
+def create_ground(width: int):
+    body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body.position = Vec2d(0, -1)
+    shape = pymunk.Poly.create_box(body, (width*2, 2))
+    shape.friction = 1.0
+    shape.color = (0, 0, 0, 255)
+    return body, shape, (0, 0, -1, width*2, 2, 0, pymunk.Body.STATIC)
+
+def random_x(minimal, maximal, random=random.Random()):
+    x = random.gauss((minimal+maximal)/2, (maximal-minimal)/6)
+    return min(max(x, minimal), maximal)
 
 
 class TestBed:
@@ -50,61 +105,92 @@ class TestBed:
         self.drawing = True
         self.random = random.Random(seed)
         self.width = width
-        self.height = height 
+        self.height = height
         self.remove = remove
         self.min_percent = min_percent
-        self.objects:list[(int,int,int)] = []
-        self.shapes:list[pymunk.Shape] = []
-        self.create_sb_world()
-        # self.create_world()
-
-    def create_world(self):
-        self.space = pymunk.Space()
-        self.space.gravity = Vec2d(0.0, -9.0)
-        self.space.sleep_time_threshold = 0.3
-
-        static_lines = [
-            pymunk.Segment(self.space.static_body, Vec2d(20, 55), Vec2d(600, 55), 1),
-            pymunk.Segment(self.space.static_body, Vec2d(550, 55), Vec2d(550, 400), 1),
-        ]
-        for l in static_lines:
-            l.friction = 0.3
-        self.space.add(*static_lines)
-
-        for x in range(5):
-            for y in range(10):
-                size = 20
-                mass = 1
-                moment = pymunk.moment_for_box(mass, (size, size))
-                body = pymunk.Body(mass, moment)
-                body.position = Vec2d(300 + x * 50, 55  + (y+0.5) * (size + 0.1))
-                shape = pymunk.Poly.create_box(body, (size, size))
-                shape.friction = 0.3
-                self.objects.append([1,body.position.x,body.position.y])
-                self.shapes.append(shape)
-                self.space.add(body, shape)
+        self.objects: list[tuple[int, ...]] = []
+        self.shapes: list[pymunk.Shape] = []
 
     def create_sb_world(self):
-        ### Init pymunk and create space
+        # Init pymunk and create space
         self.space = pymunk.Space()
         self.space.gravity = (0.0, -9.0)
         self.space.sleep_time_threshold = 0.3
         self.space.collision_slop = 0.01
-        ### ground
-        shape = pymunk.Segment(self.space.static_body, (-self.width, -1), (self.width, -1), 1.0)
-        shape.friction = 1.0
-        shape.color = (0, 0, 0, 255)
-        self.space.add(shape)
+        # ground
+        body, shape, object = create_ground(self.width)
+        self.space.add(body, shape)
+        self.shapes.append(shape)
+        self.objects.append(object)
 
-
-        #### Structure
-        self.objects,_,_ = make_structure(0,0,self.width,self.height, self.random)
+        # Structure
+        structure_objects, _, _ = make_structure(
+            0, 0, self.width, self.height, self.random)
         if self.remove:
-            total = len(self.objects)
-            min_count = min(1,int(self.min_percent * total))
-            count = self.random.randint(min_count,total)
-            self.objects = self.random.sample(self.objects, k=count)
-        self.shapes = add_itarus_objects_to_space(self.objects,self.space)
+            structure_objects = (self.filter_objects(structure_objects))
+        self.objects.extend(structure_objects)
+        self.shapes.extend(add_itarus_objects_to_space(structure_objects, self.space))
+
+    def create_random_world(self):
+        self.space = pymunk.Space()
+        self.space.gravity = (0.0, -9.0)
+        self.space.sleep_time_threshold = 0.3
+        self.space.collision_slop = 0.01
+        # ground
+        body, shape, object = create_ground(self.width)
+        self.space.add(body, shape)
+        self.shapes.append(shape)
+        self.objects.append(object)
+
+        # Create random object at top of space and let it fall
+        for i in range(random.randint(10, 40)):
+            mass = 10.0
+            random_object = self.random.choice(list(blocks.keys()))
+            dimensions = get_itarus_dimensions([int(random_object), 0, 0])
+            moment = pymunk.moment_for_box(mass, dimensions)
+            body = pymunk.Body(mass, moment)
+            # Position at top of space and random x
+            body.position = Vec2d(
+                random_x(-self.width + dimensions[0], self.width - dimensions[0], random=self.random), self.height)
+            body.rotation = self.random.uniform(0, 2 * math.pi)
+            shape = pymunk.Poly.create_box(body, dimensions)
+            shape.friction = 0.3
+            shape.color = (180, 180, 0, 255)
+            self.space.add(body, shape)
+            self.objects.append((int(random_object), int(
+                body.position.x), int(body.position.y)))
+            self.shapes.append(shape)
+            # Simulate a bit to stabilize the simulation
+            self.draw(f'tmp/add-object-{i:02}.png')
+            for _ in range(1000):
+                self.space.step(0.01)
+        # Extract objects from space
+
+        self.draw(f'tmp/intermediate.png')
+        self.objects = extract_itarus_objects_from_space(
+            self.space, self.objects)
+
+        if self.remove:
+            self.filter_objects()
+
+        # Recreate the stable scene to be able to evaluate interactions between objects
+        self.space = pymunk.Space()
+        self.space.gravity = (0.0, -9.0)
+        self.space.sleep_time_threshold = 0.3
+        self.space.collision_slop = 0.01
+
+        # Add objects
+        self.shapes = add_itarus_objects_to_space(self.objects, self.space)
+
+        self.draw(f'tmp/final.png')
+
+    def filter_objects(self, objects=None):
+        if not objects:
+            objects = self.objects
+        total = len(objects)
+        min_count = min(1, int(self.min_percent * total))
+        count = self.random.randint(min_count, total)
+        return self.random.sample(objects, k=count)
 
     def run(self):
         while self.space.current_time_step < self.max_steps:
@@ -114,7 +200,7 @@ class TestBed:
     def step(self):
         step_dt = 1 / 250
         self.space.step(self.space.current_time_step + step_dt)
-    
+
     def compare(self):
         result = []
         for object, shape in zip(self.objects, self.shapes):
@@ -134,33 +220,49 @@ class TestBed:
         # List of pairs (indexA,indexB), where indexA supports indexB
         supports = []
         generating = True
+
         def begin_handler(arbiter: pymunk.Arbiter, space: pymunk.Space, data):
             shapes = arbiter.shapes
             if generating:
-                if type(shapes[0]) == pymunk.Poly and type(shapes[1]) == pymunk.Poly:
-                    shapeA = self.get_index_of_shape(shapes[0])
-                    shapeB = self.get_index_of_shape(shapes[1])
-                    
-                    if arbiter.normal.angle_degrees > 45 and  arbiter.normal.angle_degrees < 135:
-                        supports.append((shapeA,shapeB))
-                    elif arbiter.normal.angle_degrees < -45 and  arbiter.normal.angle_degrees > -135:
-                        supports.append((shapeB,shapeA))
-                elif type(shapes[0]) == pymunk.Segment or type(shapes[1]) == pymunk.Segment:
-                    shape = self.get_index_of_shape(shapes[1]) if type(shapes[0]) == pymunk.Segment else self.get_index_of_shape(shapes[0])
-                    supports.append(("ground",shape))
-            
+                shapeA = self.get_index_of_shape(shapes[0])
+                shapeB = self.get_index_of_shape(shapes[1])
+
+                if arbiter.normal.angle_degrees > 45 and arbiter.normal.angle_degrees < 135:
+                    supports.append((shapeA, shapeB))
+                elif arbiter.normal.angle_degrees < -45 and arbiter.normal.angle_degrees > -135:
+                    supports.append((shapeB, shapeA))
+
             return True
         collision_handler._set_begin(begin_handler)
         self.step()
         generating = False
         return supports
 
-    def draw(self, file):
+    def filter_supports(self, supports):
+        # For each supporting object, check if other object would fall if supporter was removed
+        # Should return a filtered list of supports
+        # Need to copy space to not modify the original
+        result = []
+        for support in supports:
+            space = copy.deepcopy(self.space)
+            other_shape = space.shapes[support[1]]
+            other_original = self.objects[support[1]]
+            space.remove(space.shapes[support[0]])
+            for _ in range(1000):
+                space.step(0.01)
+            location_change = other_shape.body.position.get_distance((other_original[1], other_original[2]))
+            if location_change > 1:
+                result.append(support)
+        return result
+
+    def draw(self, file, space=None):
+        if not space:
+            space = self.space
         fig = plt.figure(figsize=(14, 10))
         ax = plt.axes(xlim=(-self.width, self.width), ylim=(-1, self.height))
         # ax = plt.axes(xlim=(0, 600), ylim=(0, 400))
         ax.set_aspect("equal")
         options = pymunk.matplotlib_util.DrawOptions(ax)
-        self.space.debug_draw(options)
+        space.debug_draw(options)
         fig.savefig(file, bbox_inches="tight")
         plt.close()
