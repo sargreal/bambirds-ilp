e69711470b960830d23aa13b819e37e1cac23c4a
Felix Haase <felix.haase@stud.uni-bamberg.de>
2023-08-03 08:36:05 +0000

    update data

---
+++ data/train/exs_stable_manual.pl
@@ -0,0 +1,23 @@
+% pos(stable(ground)).
+
+% completely positive instances 
+pos(stable(bg1_1_1)).
+pos(stable(bg1_2_1)).
+pos(stable(bg1_2_2)).
+pos(stable(bg1_2_3)).
+pos(stable(bg1_3_1)).
+pos(stable(bg1_3_2)).
+pos(stable(bg1_4_1)).
+pos(stable(bg1_4_2)).
+pos(stable(bg1_4_3)).
+
+% stable instances of not completely positive instances
+pos(stable(bg1_6_2)).
+pos(stable(bg1_7_2)).
+pos(stable(bg1_8_2)).
+
+% unstable instances
+neg(stable(bg1_5_1)).
+neg(stable(bg1_6_1)).
+neg(stable(bg1_7_1)).
+neg(stable(bg1_8_1)).
